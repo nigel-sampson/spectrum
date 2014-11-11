2f6dcd5c53dea8e8f244ef918ccd6826b7da7cc6
Nigel Sampson <nigel.sampson@compiledexperience.com>
2014-11-11 22:31:38 +1300

    Start of windows app

---
+++ Spectrum.Demo/Spectrum.Demo.Shared/Views/SchemeView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spectrum.Demo.Views
+{
+    public sealed partial class SchemeView
+    {
+        public SchemeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Spectrum.Demo/Spectrum.Demo.Windows/Views/EditSchemeView.xaml.cs
+++ Spectrum.Demo/Spectrum.Demo.Windows/Views/EditSchemeView.xaml.cs
@@ -13,13 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Spectrum.Demo.Views
 {
-    public sealed partial class SchemeView : UserControl
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EditSchemeView : Page
     {
-        public SchemeView()
+        public EditSchemeView()
         {
             this.InitializeComponent();
         }
